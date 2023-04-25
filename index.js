2b8ff1cfd8a4cf226d4cbd1c9c3a7cab0d794aee
saebyeolKim <toquf2611@naver.com>
2023-04-25 14:57:55 +0900

    feat: 비밀 설정 정보 관리

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded 분석해서 가져올 수 있게 해준다.
@@ -10,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://saebyeol:1234@cluster0.1jt8vzm.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
