a71c977a6459525b8e6cc1ec028645820e7347ab
saebyeolKim <toquf2611@naver.com>
2023-04-25 13:20:43 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://saebyeol:1234@cluster0.1jt8vzm.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕하세요')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
