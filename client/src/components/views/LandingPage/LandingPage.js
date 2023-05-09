61bb8e446bebc59ca9ae39d980f22e5aa28de890
saebyeolKim <toquf2611@naver.com>
2023-05-09 15:23:51 +0900

    feat: CORS이슈, Proxy 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,8 +5,8 @@ function LandingPage() {
 
   //LandingPage 들어오자마자 실행하는 것
   useEffect(() => {
-    axios.get('http://localhost:5000/api/hello') //서버에 보내는 것
-    .then(response => console.log(response.data)) //서버에서 오는 response를 콘솔창에 띄움
+    axios.get('/api/hello') //서버에 보내는 것
+    .then(response => console.log(response)) //서버에서 오는 response를 콘솔창에 띄움
   }, [])
   
 
