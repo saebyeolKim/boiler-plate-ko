9d540a9e88893764e5d5e69bc2f44fbb2e78c1ce
saebyeolKim <toquf2611@naver.com>
2023-05-10 16:22:18 +0900

    feat: 인증 체크 2

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,4 +1,3 @@
-import axios, { Axios } from "axios";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { auth } from "../_actions/user_action";
@@ -18,13 +17,32 @@ export default function (SpecificComponent, option, adminRoute = null) { //Speci
             
             dispatch(auth()).then(response => {
                 console.log(response);
+
+                //로그인 하지 않은 상태
+                if(!response.payload.isAuth) {
+                    if(option) {
+                        props.history('/login');
+                    }
+                } else {
+                    //로그인한 상태
+                    if(adminRoute && !response.payload.isAdmin) {
+                        //관리자가 아닌 사람이 관리잠 페이지를 들어가려고 할 때
+                        props.history.push('/');
+                    } else {
+                        if(option === false) {
+                            //로그인한 유저가 들어가지 못하는 페이지를 들어가려고 할 때
+                            props.history.push('/');
+                        }
+                        
+                    }
+                }
             })
         
-            Axios.length('/api/users/auth')
-          return () => {
-          }
         }, [])
         
+        return (
+            <SpecificComponent />
+        )
 
     }
 
